5e229527f6d12330c53ba4e0678de5ff2f2659ad
Gabriel Ternero <jgabriel.ts@gmail.com>
2026-08-02 13:12:44 +0200

    creación del fichero .sql

---
+++ IntroBBDD.sql
@@ -0,0 +1,11 @@
+-- Proyecto 4: SQL — Base de Datos DEMO de "Airlines"
+-- ==================================================
+
+-- 1. Escribe una consulta que recupere los Vuelos (flights) y su identificador que figuren con status On Time.
+-- Desarrollo:
+
+-- 2. Escribe una consulta que extraiga todas las columnas de la tabla bookings y refleje todas las reservas que han supuesto una cantidad total mayor a 1.000.000 (Unidades monetarias).
+-- Desarrollo:
+
+-- etc.
+
